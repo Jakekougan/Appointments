41ae4b73e4288c76a5261e7a375a67f349ffaedf
Jakekougan <jkougan@iwu.edu>
2025-05-23 16:47:17 -0500

    Added login, create account, and user authorization.

--- app.py
+++ app.py
@@ -18,7 +18,9 @@ def connect_db():
 def init_db():
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
+        sql_script = f.read()
+        print(sql_script)
+        db.cursor().executescript(sql_script)
     db.commit()
 
 @app.cli.command('initdb')
@@ -46,8 +48,58 @@ def render_form():
 
 @app.route("/login", methods=["GET"])
 def login():
-    render_template("login.html")
+    return render_template("login.html")
 
 @app.route("/create_acc", methods=["GET"])
 def create_account():
-    return render_template("create_account")
+    return render_template("create_account.html")
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    db = get_db()
+
+    fname = request.form.get("rfname")
+    lname = request.form.get("rlname")
+    email = request.form.get("remail")
+    pnum = request.form.get("rpnum")
+    pwd = request.form.get("rpwd")
+    cpwd = request.form.get("rcpwd")
+
+    if pwd != cpwd:
+        flash("Passwords do not match!")
+        return render_template("create_account.html", fname=fname, lname=lname, email=email, pnum=pnum)
+    else:
+        pwd = generate_password_hash(pwd, salt_length=11)
+        db.execute("INSERT INTO users (first_name, last_name, email, phone_number, password) VALUES (?,?,?,?,?)",
+                [fname, lname, email, pnum, pwd])
+        db.commit()
+        flash("Account created!")
+        return render_template("login.html")
+
+@app.route("/auth", methods=["POST"])
+def user_auth():
+    #Get data from login form
+    db = get_db()
+    email = request.form.get("lemail")
+    stmt = db.execute("SELECT password FROM users WHERE email = ?", [email])
+    pwd_hash = stmt.fetchone()
+    print(pwd_hash)
+    if not pwd_hash:
+        flash("User does not exist")
+        return redirect(url_for('login'))
+    else:
+        pwd = request.form.get("lpwd")
+        if check_password_hash(pwd_hash[0],pwd):
+            return render_template("index.html")
+        else:
+            flash("Your username or password in incorrect!")
+            return redirect(url_for('login'))
+
+
+
+
+
+
+
+
+
