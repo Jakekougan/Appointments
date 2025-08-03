2e707e2e083f06de75790671233f98886b1ead6c
Jakekougan <jkougan@iwu.edu>
2025-08-03 16:49:57 -0500

    Corrected issue with schema causing actions to fail.

--- schema.sql
+++ schema.sql
@@ -8,13 +8,13 @@ CREATE TABLE users (
     email VARCHAR(50) NOT NULL UNIQUE,
     phone_number CHAR(10),
     password VARCHAR(100) NOT NULL,
-
-)
+    type boolean
+);
 
 CREATE TABLE appointments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id int NOT NULL REFERENCES users(id),
     date CHAR(10),
     start_time CHAR(8),
-    end_time CHAR(8),
-)
+    end_time CHAR(8)
+);
